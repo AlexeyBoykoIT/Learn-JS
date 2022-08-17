79914d4836d6140200e0623067008e3f28ae136d
Alexey Boyko <a.boyko@anviovr.com>
2022-08-17 18:24:00 +0300

    learn js

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-const userType = prompt('Enter M for male, F for female: ');
+// const userType = prompt('Enter M for male, F for female: ');
 
-const gender = (userType == 'M') ? 'male' : 'female';
+// const gender = (userType == 'M') ? 'male' : 'female';
 
-console.log('gender:', gender);
+// console.log('gender:', gender);
+
+// function calc (num, exp) {
+//   let res = 1;
+  
+//   for (let i = 1; i <= exp; i++) {
+//     res = res * num;
+//   }
+
+//   return res;
+// }
+
+// console.log(calc(2, 4))
+
+// function even_or_odd(number) {
+//   if (number % 2 == 0) {
+//     return "Even"
+//   } else {
+//     return "Odd"
+//   }
+// }
+
+// console.log(even_or_odd(2))
+
+
+
+let year = 1705;
+console.log(year/100)
+console.log(Math.ceil(year/100))
